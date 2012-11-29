166ac6a165ab1c7b49b96b2f405b72a049e1aab6
Romulo Gomes <contato@romulodeveloper.com.br>
2012-11-29 21:28:49 -0200

    Initial web site files, just testing

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <title>Start | rômulo_devel</title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  
+  <!-- Boilerplate -->
+  <link rel="stylesheet" href="stylesheets/normalize.css">
+  <link rel="stylesheet" href="stylesheets/main.css">
+  <script src="javascripts/modernizr-2.6.1.min.js"></script>
+  <!-- CSS Theme -->
+  <link href="stylesheets/application.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="stylesheets/bootstrap.css" media="screen" rel="stylesheet" type="text/css" /> 
+  <link href="stylesheets/bootstrap-responsive.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="stylesheets/blanca.css" media="screen" rel="stylesheet" type="text/css" />
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Serif:regular,italic,bold,bolditalic" type="text/css"/>
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" type="text/css"/>
+</head>
+
+<body>
+  <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+  <![endif]-->
+	<header id="nav" class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<a href="index.php" class="brand">rômulo_devel</a>
+				<div class="nav-collapse">
+					<ul class="nav">
+						<li class="active"><a href="http://romulodeveloper.com.br/">Home</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Currículo <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#" title="Em breve">Cursos e Certificados</a></li>
+                				<li><a href="#" title="Em breve">Habilidades</a></li>
+								<li><a href="#" title="Em breve">Portfólio</a></li>
+								<li><a href="#" title="Em breve">Experiências</a></li>
+								<li><a href="#" title="Em breve">FAQ</a></li>
+							</ul>
+						</li>
+						<li><a href="features.html">Diferenciais</a></li>
+           				<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Serviços <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#" title="Em breve">Criação de Sites</a></li>
+								<li><a href="#" title="Em breve">Criação de Layouts Responsivos</a></li>
+				                <li><a href="#" title="Em breve">Desenvolvimento de MPV para Startups</a></li>
+				                <li class="divider"></li>
+								<li><a href="#" title="Em breve">SEO</a></li>
+								<li><a href="#" title="Em breve">Consultoria de UX (User Experience)</a></li>
+								<li><a href="#" title="Em breve">Consultoria em Marketing Digital</a></li>
+								<li><a href="#" title="Em breve">Campanhas de Link Patrocinado</a></li>
+								<li class="divider"></li>
+								<li><a href="#" title="Em breve">Criação de Layouts em PSD</a></li>
+								<li><a href="#" title="Em breve">Criação de Logomarcas</a></li>
+								<li><a href="#" title="Em breve">Criação de Artes em Geral</a></li>
+							</ul>
+						</li>
+						<li><a href="#" title="Em breve">Artigos</a></li>
+			            <li><a href="#" title="Em breve">Contato</a></li>
+					</ul>
+          			<!--<ul class="nav pull-right">
+						<li>
+            			  <a href="http://localhost/romulodeveloper.com.br/en/" title="English Version">
+							<img src="images/logos/english_flags_pb.png" alt="English Version" height="15px" />
+						  </a>
+						</li>
+					</ul>-->          
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+	</div>
+	<div class="stripe landing-message">
+		<div class="container">
+        <h1>.: Desenvolvimento e Otimização de Sites :.</h1>
+        <h2>tudo o de mais moderno em front-end e marketing digital está aqui ;)</h2>
+		</div>
+	</div>
+	<div class="stripe hero">
+		<div class="container">
+			<img src="images/apple-desk.jpeg" alt="web design responsivo" />
+		</div>
+	</div>
+	<div class="stripe stripe-color">
+		<div class="container">
+			<div id="download-now" class="row">
+				<div class="span12">					
+					<h2><i class="icon-star-white"> </i> Site compatível com celulares, tablets e desktops<small>, feito em HTML5, CSS3 + Less e jQuery - para nossa alegria!</small></h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="stripe">
+			<div class="container">
+				<div class="row">
+					<div class="span4">
+						<img alt="Browser" class="feature-icon" src="images/browser.png" />
+						<h2>Design Responsivo</h2>
+						<p>O acesso à internet através de celulares, tablets e até televisores já é uma realidade. Seu site precisa aparecer uniformemente em todos 
+						esses e outros dispositivos, e é aí que a criação de um site com design responsivo entra.</p>
+						<p><a class="" href="#">Veja mais &raquo;</a></p>
+					</div>
+					<div class="span4">
+						<img alt="Cloud" class="feature-icon" src="images/cloud.png" />
+						<h2>Criação de Sites</h2>
+						<p>Precisando de um novo site? Melhorar o atual? Crio sites de todos os tipos e tamanhos, de acordo com sua necessiade. Conheça mais sobre o serviço
+						ou entre em contato para solicitar seu orçamento.</p>
+						<p><a class="" href="#">Veja mais &raquo;</a></p>
+					</div>
+					<div class="span4">
+						<img alt="Music" class="feature-icon" src="images/id.png" />
+						<h2>Usabilidade e Arquitetura</h2>
+						<p>As pessoas dizem que seu site é confuso demais de utilizar? Está precisando melhorar sua taxa de conversão? Consulte o meu serviço de usabilidade
+						e aumente significativamente as vendas de seu negócio!</p>
+						<p><a class="" href="#">Veja mais &raquo;</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="stripe">
+			<div class="container">
+				<div class="row">
+					<div class="span4">
+						<img alt="Browser" class="feature-icon" src="images/browser.png" />
+						<h2>SEO</h2>
+						<p>Seu negócio precisa aparecer melhor no Google? Você tem um site muito grande e está tendo dificuldades em otimizar o seu conteúdo?
+							Conheça aqui o meu serviço de otimização de sites e aumente o tráfego e as vendas do seu site</p>
+						<p><a class="" href="#">Veja mais &raquo;</a></p>
+					</div>
+					<div class="span4">
+						<img alt="Cloud" class="feature-icon" src="images/cloud.png" />
+						<h2>Links Patrocinados</h2>
+						<p>Anuncie na internet, um mercado com exposição diária para 96% da população brasileira, além de ser o canal mais barato e mais rápido de vendas da história!
+						Crio anúncios para o Google, Yahoo! e também no Facebook.</p>
+						<p><a class="" href="#">Veja mais &raquo;</a></p>
+					</div>
+					<div class="span4">
+						<img alt="Music" class="feature-icon" src="images/id.png" />
+						<h2>Consultoria</h2>
+						<p>Você quer entrar na internet e não sabe como? Você tem uma idéia de site, mas não sabe como colocar em prática?
+						Você já possui um site e gostaria de lucrar muito mais com ele? Aqui tenho todas as respostas :)</p>
+						<p><a class="" href="#">Veja mais &raquo;</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div id="footer" class="stripe-color">
+			<div class="container">
+				<div class="row">
+					<div class="span3">
+						<h6>Telefones:</h6>
+						<address>
+							+55 11 95900-5907 (TIM)<br />
+							+55 11 98058-4648 (Oi)<br />
+							São Paulo/SP - Brasil
+					    </address>
+					</div>
+					<div class="span3">
+						<h6>Contatos Internéticos</h6>
+						<address>
+						  romulogomes.ir (skype)<br />
+						  contato@romulodeveloper.com.br<br />
+					      <a href="http://br.linkedin.com/in/romuloggomes" style="text-decoration:none;"><span style="font: Arial,sans-serif; color:#0783B6;"><img src="http://www.linkedin.com/img/webpromo/btn_in_20x15.png" width="20" height="15" alt="View Rômulo Gomes's LinkedIn profile" style="vertical-align:middle" border="0">Veja meu perfil no LinkedIn</span></a>
+						<address>
+					</div>
+					<div class="span3">
+						<h6>Back-End feito com</h6>
+						<ul>
+							<li>CodeIgniter (PHP) - Arquitetura MVC</li>
+							<li>MySQL</li>
+						</ul>
+					</div>
+					<div class="span3">
+						<h6>Front-End feito com</h6>
+						<a href="http://www.w3.org/html/logo/">
+						  <img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-device-multimedia-semantics.png" width="200" height="56" alt="HTML5 & CSS3 / Styling, Device Access, Multimedia, and Semantics" title="HTML5 & CSS3 / Styling, Device Access, Multimedia, and Semantics">
+						</a>
+					</div>
+				</div>
+				<div class="divider"></div>
+				<div class="row last">
+					<div class="span12">
+						<p class="sub">rômulo_devel@2012</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<script type="text/javascript" src="javascripts/jquery.js"></script>
+	<script type="text/javascript" src="javascripts/application.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-transition.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-alert.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-modal.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-dropdown.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-scrollspy.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-tab.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-tooltip.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-popover.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-button.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-collapse.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-carousel.js"></script>
+	<script type="text/javascript" src="javascripts/bootstrap-typeahead.js"></script>
+	<script type="textt/javascript">
+	 $(function(){
+	 	$("header a").click(function(e){
+	 		e.preventDefault();
+	 	});
+	 }
+		
+	</script>
+	
+</body>
+</html>
